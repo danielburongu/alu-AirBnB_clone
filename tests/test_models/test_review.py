8138782f9efa584bf1d8b8942c7690cbd7faba83
danielburongu <d.burongu@alustudent.com>
2023-05-19 07:57:52 +0200

    fixing errors

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_base_model
+from models.review import Review
+
+class test_review(test_base_model):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
+
+    def test_place_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.place_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
+
+    def test_text(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.text), str)
